61bedbf8a37250c067840e49f95cf9a29412ab2f
Liu <718531794@qq.com>
2020-03-22 21:20:37 +0800

    2020/03/22 日常更新

---
+++ algorithm/src/main/java/leetcode/findRepeatNumber.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * @author liuxin
+ * @version 1.0
+ * @date 2020/3/20 13:53
+ */
+public class findRepeatNumber {
+
+    class P{
+        int x;
+        int y;
+
+        public P(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<P>q = new LinkedList<>();
+        System.out.println();
+    }
+}
